86f9e0743a9a506c542e5f45bcc752a666391768
James <Jmwas@users.noreply.github.com>
2020-04-15 16:26:18 +0000

    Update Covid Stimulus.py

--- Covid Stimulus.py
+++ Covid Stimulus.py
@@ -1,41 +1,54 @@
-"""THIS IS A PROGRAM TO CALCULATE AMOUNT TO BE RECEIEVED FROM COVID-19 STIMULUS"""
-
-
-def min_refund(*args):
-    print(childAmount)
-    return
-
-
-def med_refund(*args):
+"""THIS IS A PROGRAM TO CALCULATE AMOUNT TO BE RECEIVED FROM COVID-19 STIMULUS"""
+def min_refund(kids):
+    return kids * 500
+def med_refund(n, income, kids):
+    child_amount = kids * 500
     if n == 's':
-        print(round(singleAmount - ((income - 75000) / 100) + childAmount))
+        return round(amountByMaritalStatus['single'] - (((income - 75000) / 100)*5) + child_amount)
 
     else:
-        print(round(marriedAmount - ((income - 150000) / 100) + childAmount))
+        return round(amountByMaritalStatus['married'] - (((income - 150000) / 100)*5) + child_amount)
 
 
-def max_refund(*args):
+def max_refund(n,kids):
+    child_amount = kids * 500
     if n == 's':
-        print(singleAmount + childAmount)
+        return amountByMaritalStatus['single'] + child_amount
 
     else:
-        print(marriedAmount + childAmount)
+        return amountByMaritalStatus['married'] + child_amount
 
 
-n = input('Enter Your Filing Status. S for single, Head of Household or Married filing Separate and  M for married: ').lower()
-income = int(input('Enter your 2018 Federal Income: '))
-kids = int(input('Enter the number of dependents under 17: '))
+def get_status():
+    if (n == 's' and income > 99000) or (n == 'm' and income > 198000):
+        print(f'Your stimulus refund is', min_refund(kids))
 
-singleAmount = 1200
-marriedAmount = 2400
-childAmount = 500 * kids
+    elif (n == 's' and income > 75000) or (n == 'm' and income > 150000):
+        print(f'Your stimulus refund is', med_refund(n, income, kids))
+
+    else:
+        print(f'Your refund is', max_refund(n, kids))
 
 
-if (n == 's' and income > 99000) or (n == 'm' and income > 198000):
-    min_refund()
+if __name__ == '__main__':
+    amountByMaritalStatus = {'single': 1200, 'married': 2400}
 
-elif (n == 's' and income > 75000) or (n == 'm' and income > 150000):
-    med_refund()
+    while True:
+        n = input('Enter Your Filing Status. S for single, Head of Household or Married filing Separate and \
+M for married: ').lower()
 
-else:
-    max_refund()
+        if n not in ('s', 'm'):
+            print('Please enter S or M')
+            continue
+        try:
+            income = int(input('Enter your 2019 Federal Income: '))
+        except ValueError:
+            print('Please enter a number')
+            continue
+        try:
+            kids = int(input('Enter the number of dependents under 17: '))
+        except ValueError:
+            print('Please enter a number')
+            continue
+        get_status()
+        break
